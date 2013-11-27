163280f0e1e054755469c40d1a38bfa3e007a411
Dave McLain <dmclain@gmail.com>
2013-11-27 17:46:47 -0600

    beta

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-toolbar-line-profiler',
-    version='0.1.0',
+    version='0.2.0.beta',
     description='A panel for django-debug-toolbar that integrates ' +
                 'information from line_profiler',
     long_description=open('README.rst').read(),
