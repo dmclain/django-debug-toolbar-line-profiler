e9863b299faea8d4a682dd8f8da182ef98cd4224
Dave McLain <dmclain@gmail.com>
2016-03-03 00:07:02 +0000

    v0.6.1 versioning is hard

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-toolbar-line-profiler',
-    version='0.5.2',
+    version='0.6.1',
     description='A panel for django-debug-toolbar that integrates ' +
                 'information from line_profiler',
     long_description=open('README.rst').read(),
